7ebf07c23f7378fa924f04360ecba8d82239d42f
Aminul Islm <ai8728340@gmail.com>
2025-09-01 23:07:10 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hrms-v2/',
-  server: {
-    open: '/hrms-v2/'
-  }
+  
 })
